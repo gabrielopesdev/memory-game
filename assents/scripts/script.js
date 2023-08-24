5e0d6aa531d16304d42aa6ea450fe8ae46bce72d
Gabriel dos Santos <109832618+gabrielopesdev@users.noreply.github.com>
2023-08-24 23:44:15 +0000

    createCards

--- assents/scripts/script.js
+++ assents/scripts/script.js
@@ -1,1 +1,55 @@
-console.log("penis")
+const front = "card_front"
+const back = "card_back"
+
+let techs = [
+
+    'bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'javascript',
+    'jquary',
+    'mongo',
+    'node',
+    'react'
+];
+
+createCardsFromTechs(techs)
+
+function createCardsFromTechs(techs) {
+
+    let cards = []
+
+    for (let tech of techs) {
+
+        cards.push(createPairFromTech(tech))
+
+    }
+    
+    return cards.flatMap(pair => pair)
+}
+
+
+function createPairFromTech(tech) {
+
+    return [{
+        
+        id: createIdFromTech(tech),
+        icon: tech,
+        flipped: false,
+
+    }, {
+
+        id: createIdFromTech(tech),
+        icon: tech,
+        flipped: false,
+
+    }]
+}
+
+function createIdFromTech(tech) {
+
+    return tech + parseInt( Math.random() * 1000)
+
+}
